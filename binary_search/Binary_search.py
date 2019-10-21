abd1da48d4920ed288da08ba0e3044afec8a487a
AnukiAlwis <56637505+AnukiAlwis@users.noreply.github.com>
2019-10-21 12:27:42 +0530

    Add files via upload

--- binary_search/Binary_search.py
+++ binary_search/Binary_search.py
@@ -1,34 +1,42 @@
-array = []
-for i in range(11):
-    a = int(input("enter num :"))
+array=[]
+Num=1
+
+Num= int (input("Enter the length of your array : "))
+
+for i in range(Num):
+    a = int(input("enter array items :"))
     b = array.append(a)
-print("Array --------> ", array)
+print("Array --------> " ,array)
 
-for i in range(len(array) - 1, 0, -1):
+for i in range(len(array)-1,0,-1):
     for j in range(i):
-        if (array[j] > array[j + 1]):
+        if (array[j] > array[j+1]):
             temp = array[j]
-            array[j] = array[j + 1]
-            array[j + 1] = temp
+            array[j] = array[j+1]
+            array[j+1] = temp
 
-print("\nSorted Array --------> ", array)
+print("\nSorted Array --------> " ,array)
 
-c = int(input("\nEnter search num :"))
+c = int (input("\nEnter search num :"))
+new_array=[]
+if(c > array[len(array)/2]):
+    for k in range((len(array)/2+1),len(array)+1):
+        d = new_array.append(array[k])
+    #print(new_array)
+    for l in range(Num):
+        if (array[l] == c):
+            print("search is finished. Index is :", l)
 
-aLen = len(array)
-t = aLen // 2
+elif(c == array[len(array)/2]):
+    print("search is finished. Index is :", len(array)/2)
 
-while((t != -1) and (t < aLen)):
-    if(c > array[t]):
-        t = (t + aLen + 1) // 2
-    elif(c < array[t]):
-        t = t // 2
-    elif(c == array[t]):
-        break
-    else:
-        t = aLen + 1
-
-if(t < aLen):
-    print("search is finished. Index is :", t)
 else:
-    print("search is finished.Value Not found.")
+    for k in range(len(array)/2):
+        e = new_array.append(array[k])
+    print(new_array)
+    for l in range(len(array)+1):
+        if (array[l] == c):
+            print("search is finished. Index is :", l)
+        
+
+        
